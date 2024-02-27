909a84a8e1dc4bbb6783cc441df4c88750c4f739
eudus <eudusb@shaw.ca>
2024-02-26 20:07:00 -0800

    Added code

---
+++ src/add.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "Enter two numbers below\n";
+	double num1 = 0.0, num2 = 0.0;
+	std::cout << "Enter the first number: ";
+	std::cin >> num1;
+	std::cout << "Enter the second number: ";
+	std::cin >> num2;
+	std::cout << "Their sum is: " << num1+num2 << "!\n";
+	return 0;
+}
